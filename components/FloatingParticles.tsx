ab829a55e484cbca6bd7d4bbed44a23944ebb9bc
Matias Psara <matiaspsara@gmail.com>
2025-08-11 10:46:02 +0200

    fix components use in homepage

---
+++ components/FloatingParticles.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function FloatingParticles() {
+  return (
+    <div className="absolute inset-0 overflow-hidden pointer-events-none">
+      {[...Array(20)].map((_, i) => (
+        <div
+          key={i}
+          className="absolute w-2 h-2 bg-white/20 rounded-full animate-pulse"
+          style={{
+            left: `${Math.random() * 100}%`,
+            top: `${Math.random() * 100}%`,
+            animationDelay: `${Math.random() * 2}s`,
+            animationDuration: `${3 + Math.random() * 2}s`
+          }}
+        />
+      ))}
+    </div>
+  );
+}
